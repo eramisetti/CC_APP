1f6a5ad2e3ac7287382729ec08c05ca9d31bf90f
eramisetti <eramisetti@gmail.com>
2019-01-23 23:57:45 -0800

    angular 7 commit

--- cc-web/src/main/web/README.md
+++ cc-web/src/main/web/README.md
@@ -1,6 +1,6 @@
-# NpApp
+# Ang7route
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.0.
 
 ## Development server
 
--- cc-web/src/main/web/src/app/app.module.ts
+++ cc-web/src/main/web/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { StudentModule } from './student/student.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StudentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
