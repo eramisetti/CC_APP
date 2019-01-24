93405df2f9b2b3b15493916e4c9b101be0d2147d
eramisetti <eramisetti@gmail.com>
2019-01-23 23:09:11 -0800

    security disabled

--- cc-server/src/main/java/com/cc/springboot/controller/HomeController.java
+++ cc-server/src/main/java/com/cc/springboot/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/api/home")
 public class HomeController {
 	
 	@GetMapping
