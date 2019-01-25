622a05d9877989bd4b5563c90eb0ea7310163a9d
eramisetti <eramisetti@gmail.com>
2019-01-25 00:26:06 -0800

    log in pages

--- cc-web/src/main/web/src/app/app.module.ts
+++ cc-web/src/main/web/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { StudentModule } from './student/student.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     HomeComponent
   ],
   imports: [
